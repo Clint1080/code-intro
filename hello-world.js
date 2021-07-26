21f54541773c50a361c83f6bba4945dc0cdce5c2
Clint Edwards <cedwards1080@gmail.com>
2021-07-26 14:18:46 -0600

    Finished the lecture

--- hello-world.js
+++ hello-world.js
@@ -1,19 +1,56 @@
 // This is our first javascript comment
+const readline = require("readline");
 
 //  here is some text for fun
-console.log("Hello World");
+// console.log("Hello World");
 
-let message = "Hello again, world";
-console.log(message);
+const firstName = "Clint"
+let age = 35;
 
+// console.log(firstName,age);
 
-const readline = require("readline");
+// age = 34;
+
+// console.log(age);
+
+const isCool = true
+
+let whatImEatingForLunch = "sandwhich"
+
+// console.log(firstName, age, isCool, whatImEatingForLunch);
+
+let aVar = null;
+
+// console.log(age + aVar);
+
+let x = 4
+let y = 10
+let z = 7 + 8
+
+let sum = x + y + z
+// console.log(sum);
+
+let str = "StringOne"
+let str2 = 'StringTwo'
+let str3 = `StringThree`
+
+let combinedStrings = str + ", " + str2
+
+// console.log(combinedStrings); 
+
+let templateLiterals = `hello, ${firstName}`
+// console.log(templateLiterals)
 
 const reader = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
-});
+  output: process.stdout
+})
 
-reader.question("Howdy, what's your name?", function(answer) {
-  console.log("Hi " + answer);
+reader.question("Do you like cats y/n?", function(answer) {
+  if(answer === 'y') {
+      console.log("that's fine I guess"
+      )
+  } else {
+      console.log("We'll get along fine")
+  }
 });
